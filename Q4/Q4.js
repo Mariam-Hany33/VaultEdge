59cd6baca4e7d633e469913a28567d19fff82cf8
Mariam Hany <mariamhanymakram@gmail.com>
2026-08-19 10:32:15 +0300

    Delete Q4 directory

--- Q4/Q4.js
+++
@@ -1,7 +0,0 @@
-let x = Number(prompt("Enter a number:"));
-if (x % 2 === 0) {
-    document.getElementById("E-O").innerHTML = "Even" ;
-} else {
-    document.getElementById("E-O").innerHTML = "Odd" ;
-}
-
